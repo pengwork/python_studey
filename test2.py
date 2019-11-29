5ee6fdbc76f53b1c013811e217861bf658a66ea8
pengwork <31396670+pengwork@users.noreply.github.com>
2019-11-29 23:04:41 +0800

    输出一个数阶乘末尾零的个数

---
+++ test2.py
@@ -0,0 +1,18 @@
+# def fact(n):
+#     if n%5 == 0 and n >= 5:
+#         return
+#     else:
+#         return n + fact(n-1)
+if __name__ == '__main__':
+    n = 1001171717
+    number = n/5
+    # print(number)
+    # num = str(number)
+    # count = 0
+    # i = 0
+    # while num[len(num) - 1 - i] == '0':
+    #     count += 1
+    #     i+=1
+
+    print(number)
+
